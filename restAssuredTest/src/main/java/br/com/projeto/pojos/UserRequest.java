51b0a77cb24ae504d679204779828e2fe53acfb3
Marcus Berger <marcusvcberger@gmail.com>
2023-09-13 21:21:08 -0300

    add lombok

--- restAssuredTest/src/main/java/br/com/projeto/pojos/UserRequest.java
+++ restAssuredTest/src/main/java/br/com/projeto/pojos/UserRequest.java
@@ -1,36 +1,17 @@
 package br.com.projeto.pojos;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class UserRequest {
+
     @JsonProperty("name")
     private String name;
 
     @JsonProperty("job")
     private String job;
 
-
-    public UserRequest() {
-    }
-
-    public UserRequest(String name, String job) {
-        this.name = name;
-        this.job = job;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getJob() {
-        return job;
-    }
-
-    public void setJob(String job) {
-        this.job = job;
-    }
 }
--- restAssuredTest/src/main/java/br/com/projeto/pojos/UserResponse.java
+++ restAssuredTest/src/main/java/br/com/projeto/pojos/UserResponse.java
@@ -1,7 +1,11 @@
 package br.com.projeto.pojos;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class UserResponse {
     @JsonProperty("id")
     private int id;
@@ -15,47 +19,4 @@ public class UserResponse {
     @JsonProperty("createdAt")
     private String createdAt;
 
-    // Construtores, getters e setters
-
-    public UserResponse() {
-    }
-
-    public UserResponse(int id, String name, String job, String createdAt) {
-        this.id = id;
-        this.name = name;
-        this.job = job;
-        this.createdAt = createdAt;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getJob() {
-        return job;
-    }
-
-    public void setJob(String job) {
-        this.job = job;
-    }
-
-    public String getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(String createdAt) {
-        this.createdAt = createdAt;
-    }
 }
